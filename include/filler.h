b79215d296366263452735899fbc4a543c9c8bdb
nicovrg <nicolasvergne88@gmail.com>
2019-04-16 13:41:02 +0200

    get player id, map dimensions and allocate the map

--- include/filler.h
+++ include/filler.h
@@ -6,18 +6,19 @@
 /*   By: nivergne <nivergne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/15 15:09:38 by nivergne          #+#    #+#             */
-/*   Updated: 2019/04/15 19:17:35 by nivergne         ###   ########.fr       */
+/*   Updated: 2019/04/16 13:39:37 by nivergne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef _FILLER_H
 # define _FILLER_H
 
-typedef struct  s_map {
-    int     player_id;
-    int     map_width;
-    int     map_height;
-}               t_map;
+typedef struct	s_map {
+	char	**map;
+	int		map_width;
+	int		map_height;
+	int		player_id;
+}				t_map;
 
 
 #endif
--- src/main.c
+++ src/main.c
@@ -6,60 +6,100 @@
 /*   By: nivergne <nivergne@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/15 14:13:11 by nivergne          #+#    #+#             */
-/*   Updated: 2019/04/15 19:20:29 by nivergne         ###   ########.fr       */
+/*   Updated: 2019/04/16 13:39:35 by nivergne         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 #include "filler.h"
 
-int     check_info_player(t_map *map)
+int		init_struct(t_map *map)
 {
-    char *line;
-
-    line = NULL;
-    if (!get_next_line(0, &line) != 1)
-        return (0);
-    if (!strncmp(line, "$$$ exec p", 10))
-    {
-        ft_strdel(&line);
-        return (0);
-    }
-    if (!line[10] || (line[10] && line[10] != "1" && line[10] != "2"))
-    {
-        ft_strdel(&line);
-        return (0);
-    }
-    map->player_id = line[10] == "1" ? 1 : 2;
-    ft_strdel(&line);
-    return (1);
+	if (!get_player_id(map))
+	{
+		write(2, "player_id incorrect\n", 20);
+		return (0);
+	}
+	if (!get_map_dimensions(map))
+	{
+		write(2, "map_dimensions incorrect\n", 25);
+		return (0);
+	}
+	if (!allocate_map(map))
+	{
+		write(2, "error during map allocation\n", 28);
+		return (0);
+	}
+	return (1);
 }
 
-int     check_info_map(t_map *map)
+int		get_player_id(t_map *map)
 {
-    int i;
-    char *line;
+	char *line;
 
-    i = 0;
-    line = NULL;
-    if (!get_next_line(0, &line) != 1)
-        return (0);
-    while (line[i] && !ft_isdigit(line[i]))
-        i++;
-    map->map_height = ft_atoi(line + i);
-    while (line[i] && !ft_isdigit(line[i]))
-        i++;
-    map->map_width = ft_atoi(line + i);    
-    ft_strdel(&line);
-    return (1);
+	line = NULL;
+	if (!get_next_line(0, &line) != 1)
+		return (0);
+	if (!strncmp(line, "$$$ exec p", 10))
+	{
+		ft_strdel(&line);
+		return (0);
+	}
+	if (!line[10] || (line[10] && line[10] != "1" && line[10] != "2"))
+	{
+		ft_strdel(&line);
+		return (0);
+	}
+	map->player_id = line[10] == "1" ? 1 : 2;
+	ft_strdel(&line);
+	return (1);
 }
 
-int     main(void)
+int		get_map_dimensions(t_map *map)
 {
-            
-    while (1)
-    {
+	int i;
+	char *line;
 
-    }
-    return (0);
+	i = 0;
+	line = NULL;
+	if (!get_next_line(0, &line) != 1)
+		return (0);
+	while (line[i] && !ft_isdigit(line[i]))
+		i++;
+	map->map_height = ft_atoi(line + i);
+	while (line[i] && !ft_isdigit(line[i]))
+		i++;
+	map->map_width = ft_atoi(line + i);
+	ft_strdel(&line);
+	return (1);
+}
+
+int		allocate_map(t_map *map)
+{
+	int i;
+
+	i = 0;
+	if (!(map->map = (char **)malloc(sizeof(char *) * (map->map_height + 1))))
+			return (0);
+	while (i < map->map_height)
+	{
+		if (!(map->map[i] = (char *)malloc(sizeof(char) * (map->map_width + 1))))
+			return (0);
+		i++;
+	}
+	map->map[map->map_height] = 0;
+	return (0);
+}
+
+int		main(void)
+{
+	t_map	map;
+
+	if (!init_struct(&map))
+		return (0);
+	while (1)
+	{
+
+	}
+	return (0);
 }
